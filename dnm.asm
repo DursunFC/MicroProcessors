1a9aecde9942c83b26bebefb7144424cb8285420
Furkan Can Dursun <115872199+DursunFC@users.noreply.github.com>
2025-03-17 09:16:57 +0300

    Add files via upload

---
+++ dnm.asm
@@ -0,0 +1,22 @@
+.model small
+
+.data
+
+.code
+
+main proc
+    mov cx,0
+    mov al,1
+    mov ah,2
+    mov bl,3
+    mov bh,4
+    mov cl,5
+    mov ch,6
+    mov dl,7
+    mov dh,8 
+       
+
+QUIT:hlt
+
+endp
+end main
---
+++ template.asm
@@ -0,0 +1,12 @@
+.model small
+
+.data
+
+.code
+
+main proc
+     mov cx,0 
+
+    hlt
+endp
+end main
